ca7b7d42429bb8db1d7d0be81b78eb5b51189f84
2vans <mock.jablonski@gmail.com>
2017-06-28 10:52:07 +0200

    Initial Commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+* This is the first file loaded by the web server.
+* It prints some messages and html from other files.
+*/
+
+
+$firstname = 'Hiro';
+$surname = 'Nakamura';
+echo "My name is $firstname $surname.</br> I am a master \nof time and
+space. \"Yatta!\"";
+?>
